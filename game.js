28c45b0ef09febc5a1b62c0c9a83ea0ed962b51d
Martin Németh <martin@revolin.io>
2022-11-02 16:36:03 +0100

    Add clouds

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ const config = {
         default: 'arcade',
         arcade: {
             debug: false,
-            gravity: { y: 300 }
+            gravity: { y: 500 }
         }
     },
     scene: {
@@ -24,6 +24,7 @@ const game = new Phaser.Game(config);
 
 function preload() {
     this.load.image("grass", "assets/sprites/grass.png")
+    this.load.image("cloud", "assets/sprites/cloud.png")
     this.load.spritesheet("midget", "assets/sprites/midget.png", 
     {frameWidth: 390, frameHeight: 429});
 }
@@ -35,26 +36,38 @@ function create() {
     for (let i=0;i<4;i++) {
         platforms.create(100 + i * 200, 575, 'grass');
     }
+
+    platforms.create(200, 150, 'cloud');
+    platforms.create(430, 300, 'cloud');
+    platforms.create(600, 220, 'cloud');
+    platforms.create(50, 500, 'cloud');
+    platforms.create(750, 500, 'cloud');
     
-    midget = this.physics.add.sprite(50, 500, 'midget').setScale(0.2);
-    midget.setCollideWorldBounds(true);
+    midget = this.physics.add.sprite(150, 50, 'midget').setScale(0.15);
 
     this.physics.add.collider(midget, platforms)
-
+    
     this.anims.create({
         key: "walk",
         frames: "midget",
         frameRate: 10,
         repeat: -1
     });
+
 }
 
 function update() {
-    midget.anims.play("walk", true);
-    console.log(midget.body.left);
+    if (midget.body.touching.down) midget.anims.play("walk", true);
+    else midget.anims.play("walk", false)
+
     if (midget.body.velocity.x === 0) midget.body.velocity.x = 100;
-    if(midget.body.right >= config.width-1 || midget.body.left <= 1) {
-        midget.body.velocity.x = -midget.body.velocity.x;
-        midget.flipX = !midget.flipX;
+
+    if (midget.body.touching.left) {
+        midget.body.velocity.x = 100;
+        midget.flipX = false;
+    }
+    if (midget.body.touching.right) {
+        midget.body.velocity.x = -100;
+        midget.flipX = true;
     }
 }
