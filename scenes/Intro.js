45213801504e0a6f446789fb00c34600d900f1ce
Martin Németh <martin@revolin.io>
2022-11-29 12:24:59 +0100

    Background refactor

--- scenes/Intro.js
+++ scenes/Intro.js
@@ -8,8 +8,8 @@ class Intro extends Phaser.Scene {
 
     preload() {
 
-        this.load.image("intro_background", "assets/sprites/intro_background.png");
-        this.load.image("menu_background", "assets/sprites/menu_background.png");
+        this.load.image("intro_background", "assets/backgrounds/intro.png");
+        this.load.image("menu_background", "assets/backgrounds/menu.png");
         this.load.image("grass", "assets/sprites/grass.png");
         this.load.image("cloud", "assets/sprites/cloud.png");
         this.load.image("play", "assets/sprites/play.png");
@@ -22,7 +22,7 @@ class Intro extends Phaser.Scene {
         this.load.image("arrow", "assets/sprites/arrow.png");
         this.load.image("stone", "assets/sprites/stone.png");
 
-        this.load.image("cave_background", "assets/sprites/cave_background.png");
+        this.load.image("cave_background", "assets/backgrounds/cave.png");
         this.load.image("jelly", "assets/sprites/jelly.png");
         this.load.audio("boing1", "assets/sounds/boing1.mp3");
         this.load.audio("boing2", "assets/sounds/boing2.mp3");
@@ -30,17 +30,20 @@ class Intro extends Phaser.Scene {
         this.load.image("falls_tiles", "assets/tilemaps/falls.png");
         this.load.tilemapTiledJSON("falls", "assets/tilemaps/falls.json");
         this.load.audio("waterfall", "assets/sounds/waterfall.mp3");
-        this.load.image("waterfall_background", "assets/sprites/waterfall_background.png");
+        this.load.image("waterfall_background", "assets/backgrounds/waterfall.png");
         this.load.image("lilypads", "assets/sprites/lilypads.png");
 
         this.load.image("dungeon_tiles", "assets/tilemaps/dungeon.png");
         this.load.tilemapTiledJSON("dungeon", "assets/tilemaps/dungeon.json");
-        this.load.image("dungeon_background", "assets/sprites/dungeon_background.png");
+        this.load.image("dungeon_background", "assets/backgrounds/dungeon.png");
         this.load.audio("unlock", "assets/sounds/unlock.mp3");
 
         this.load.image("dungeon_tiles", "assets/tilemaps/dungeon.png");
         this.load.tilemapTiledJSON("tower", "assets/tilemaps/tower.json");
+        this.load.image("tower_background", "assets/backgrounds/tower.png");
         this.load.image("dark_cloud", "assets/sprites/dark_cloud.png");
+        this.load.image('purple', 'assets/particles/purple.png');
+        this.load.image('star', 'assets/particles/star.png');
     }
 
     create() {
